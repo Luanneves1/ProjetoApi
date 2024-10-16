a8aead2f84ace8b5590f73fcd79ac075f7ff2c61
Luan Neves <luanpneves@gmail.com>
2024-10-16 20:51:48 -0300

    ProjetoApi

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
+    res.status(201).json
+
+});
+
+app.get('/usuarios', async (req, res) => {
+    const Users = await prisma.user.findMany();
+    res.status(200).json(Users);
+
+});
+
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
+    res.status(201).json
+
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(201).json({ mensagem: 'Usuario deletado com sucesso' })
+
+});
+
+
+app.listen(3000);
+
